32022eea5dbcbb79b668362a669d832664108d04
Danilo Oliveira <141935869+Danilitoxp@users.noreply.github.com>
2023-10-31 16:53:14 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const generateDescriptionButton = document.getElementById("generateDescriptionButton");
+    const copyDescriptionButton = document.getElementById("copyDescriptionButton"); // Adicionado botão de cópia
+    const productCodeInput = document.getElementById("productCode");
+    const productDescription = document.getElementById("productDescription");
+
+    generateDescriptionButton.addEventListener("click", function() {
+        const code = productCodeInput.value;
+        const description = generateProductDescription(code);
+        productDescription.innerHTML = description;
+    });
+
+    copyDescriptionButton.addEventListener("click", function() {
+        const descriptionText = productDescription.innerText;
+        copyToClipboard(descriptionText);
+    });
+
+    function generateProductDescription(code) {
+        const parts = code.split(" ");
+        const title = parts.slice(0, parts.length - 4).join(" ");
+        const yearRange = parts.slice(parts.length - 4, parts.length - 1).join(" ");
+        const sizes = parts[parts.length - 1].split(",");
+    
+        const sizeDriver = sizes[0].trim();
+        const sizePassenger = sizes[1].trim();
+    
+        const compatibleYears = generateCompatibleYears(title, yearRange);
+
+        const description = `Par Palheta Limpador Para Brisa ${title} Silicone<br><br>
+            Nossas Palhetas, além de contar com haste flexível e corpo todo emborrachado, o que impede impactos que possam vir a ocorrer com o vidro, essas palhetas de para-brisa oferecem uma instalação rápida e segura para veículos com encaixe tipo Gancho.<br><br>
+            Tipo do encaixe:<br>Gancho<br><br>
+            Conteúdo da Embalagem:<br><br>
+            - 01 Palheta ${sizeDriver} Polegadas Lado Motorista<br>
+            - 01 Palheta ${sizePassenger} Polegadas Lado Passageiro<br><br>
+            Valor do Par<br><br>
+            Compatível com:<br><br>${compatibleYears}<br><br>
+            Especificações Técnicas:<br><br>
+            - em Plástico Maleável<br>
+            - Sem Partes Metálicas Expostas<br>
+            - Revestimento em Borracha que Reduz a Fricção com o Vidro<br>
+            - Limpeza Silenciosa<br>
+            - Adaptador Pré-Montado para Uma Fácil Instalação<br>
+            - Resiste às Mais Diversas Condições Climáticas<br>
+            - Longa Vida Útil<br>
+            - Para Veículos com Braço Tipo Gancho<br>
+            - Substitui a Palheta Convencional<br>
+            - Instalação Rápida e Segura<br><br>
+            Importante:<br><br>
+            Garantia de 30 Dias.`;
+    
+        return description;
+    }
+
+    function generateCompatibleYears(title, yearRange) {
+        const [startYear, endYear] = yearRange.split(" a ");
+        const yearList = [];
+
+        for (let year = parseInt(startYear); year <= parseInt(endYear); year++) {
+            yearList.push(`${title} ${year}`);
+        }
+
+        return yearList.join("<br>");
+    }
+
+    function copyToClipboard(text) {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textArea);
+    }
+});
